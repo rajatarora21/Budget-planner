7a820a3bec7fc65450dfec95d3fdbfb445aed65f
ra_aro <ra_aro@live.concordia.ca>
2023-08-08 22:12:13 +0530

    budget-planner

---
+++ react-budget-planner/src/actions/logout.js
@@ -0,0 +1,17 @@
+// rrd imports
+import { redirect } from "react-router-dom";
+
+// helpers
+import { deleteItem } from "../helpers";
+
+import { toast } from "react-toastify";
+export async function logoutAction() {
+  // delete the user
+  deleteItem({
+    key: "userName"
+  })
+
+  toast.success("Deleted")
+  // return redirect
+  return redirect("/")
+}
---
+++ react-budget-planner/src/pages/Dashboard.jsx
@@ -0,0 +1,52 @@
+// rrd imports
+import { useLoaderData } from "react-router-dom";
+
+//  helper functions
+import { fetchData } from "../helpers"
+import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
+import { toast } from "react-toastify";
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  const budgets = fetchData("budgets");
+  return { userName, budgets }
+}
+
+export async function dashboardAction({request}){
+    const data = await request.formData();
+    const formData = Object.fromEntries(data)
+    try{
+        localStorage.setItem("userName", JSON.stringify(formData.userName))
+        return toast.success('Welcome ' + formData.userName)
+    }
+    catch(e)
+    {
+        throw new Error("There was a problem in account creation")
+    }
+    
+}
+const Dashboard = () => {
+  const { userName, budgets } = useLoaderData()
+
+  return (
+    <div>
+     {userName ? (
+        <div className="dashboard">
+            <h1>Welcome back, <span className="accent">{userName}</span></h1>
+            <div className="grid-sm">
+                <div className="grid-lg">
+                    <div className="flex-lg">
+                        <AddBudgetForm/>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+     ) : (<Intro />)}
+    </div>
+  )
+}
+export default Dashboard
